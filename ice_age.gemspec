25f0518be380674fa12d71c5e03f07d5d8be83e3
Daniel Pepper <pepper.daniel@gmail.com>
2023-02-12 22:07:27 -0800

    codecov upgrade

--- ice_age.gemspec
+++ ice_age.gemspec
@@ -14,7 +14,6 @@ Gem::Specification.new do |s|
   s.files       = `git ls-files * ":!:spec"`.split("\n")
 
   s.add_development_dependency "byebug"
-  s.add_development_dependency "codecov"
   s.add_development_dependency "rspec"
   s.add_development_dependency "simplecov"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
 require 'byebug'
-
 require 'simplecov'
+
 SimpleCov.start do
   add_filter /spec/
 end
 
 if ENV['CI'] == 'true' || ENV['CODECOV_TOKEN']
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  require 'simplecov_json_formatter'
+  SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 end
 
 require 'ice_age'
